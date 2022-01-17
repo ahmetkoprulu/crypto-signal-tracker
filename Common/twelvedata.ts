9227b9d88251e7199a01b41194e5013af678a113
ahmetkoprulu <aensarkoprulu@gmail.com>
2022-01-17 21:21:22 +0300

    Added wrapper class for fetching indicator data from Twelvedata

---
+++ Common/twelvedata.ts
@@ -0,0 +1,70 @@
+import axios, { Method } from "axios";
+
+axios.defaults.baseURL = "https://api.twelvedata.com";
+axios.defaults.headers.post["Content-Type"] = "application/json";
+
+export default class RequestBuilder {
+  key: string;
+
+  constructor(key: string) {
+    this.key = key;
+  }
+
+  // TODO: implement complex data.
+  technicalAnalysis(indicator: string | Array<string>): RequestEndPoint {
+    return new RequestEndPoint(this.key, `/${indicator}`, "get");
+  }
+
+  static requestFinancialData() {
+    let key = process.env.TWELVEDATA_SECRET_KEY;
+    if (!key) throw new Error("Missing key");
+
+    return new RequestBuilder(key);
+  }
+}
+
+class RequestEndPoint implements IRequestBase {
+  url: string;
+  method: Method;
+  params: any;
+
+  constructor(key: string, url: string, method: Method) {
+    this.url = url;
+    this.method = method;
+    this.params = { apikey: key };
+  }
+
+  symbol(symbol: string): RequestEndPoint {
+    this.params.symbol = symbol;
+
+    return this;
+  }
+
+  interval(interval: string): RequestEndPoint {
+    this.params.interval = interval;
+
+    return this;
+  }
+
+  batchRequest(symbols: Array<string>): RequestEndPoint {
+    this.params.symbol = symbols.join(",");
+
+    return this;
+  }
+
+  async request() {
+    console.log(this.url, this.method);
+    return axios.request({
+      url: this.url,
+      method: this.method,
+      params: this.params,
+    });
+  }
+}
+
+interface IRequestBase {
+  url: string;
+  method: string;
+
+  request(): any;
+}
